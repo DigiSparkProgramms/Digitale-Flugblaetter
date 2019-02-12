7db4e93cfa4e53862320cde4464e240667d1204a
DigiSparkProgramms <47527747+DigiSparkProgramms@users.noreply.github.com>
2019-02-12 17:40:33 +0100

    Update Digitales_Flugblatt_US_T0.ino

--- Digitales_Flugblatt_US_T0.ino
+++ Digitales_Flugblatt_US_T0.ino
@@ -4,7 +4,7 @@ void setup() {
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(3000);
-  DigiKeyboard.print("https://enough-is-enough14.org/wp-content/uploads/2019/01/schulstreikflyer.jpg");
+  DigiKeyboard.print("https://enough-is-enough14.org/wp-content/uploads/2019/01/schulstreikflyer.jpg");//öffnet ein Bild im Internet
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(2000);
   DigiKeyboard.sendKeyStroke(KEY_F11);
