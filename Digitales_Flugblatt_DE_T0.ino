e3c54c6a1f96922b2c7e3d7b78c6b11e5d76cb81
DigiSparkProgramms <47527747+DigiSparkProgramms@users.noreply.github.com>
2019-03-05 22:18:39 +0100

    Update Digitales_Flugblatt_DE_T0.ino

--- Digitales_Flugblatt_DE_T0.ino
+++ Digitales_Flugblatt_DE_T0.ino
@@ -4,7 +4,7 @@ void setup() {
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(3000);
-  DigiKeyboard.print("https>&&enough/is/enough14.org&wp/content&uploads&2019&01&schulstreikflzer.jpg"); //öffnet einen Link  (zu einem Bild) im Internet
+  DigiKeyboard.print("https>&&www.zoutube.com&embed&D1monu7dsoZ_autoplaz)1^loop)1^fullscreen)1^plazlist)D1monu7dsoZ"); //öffnet einen Link  (zu einem Bild) im Internet
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(2000);
   DigiKeyboard.sendKeyStroke(KEY_F11); // Maximiert das Bild
